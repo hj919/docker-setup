45f437bb18166ea2d30ba8126a1ed322113ac914
jie <2273828010@qq.com>
2018-03-17 23:02:00 +0800

    修改时区bug

--- init.sh
+++ init.sh
@@ -93,15 +93,15 @@ function createContainer(){
 			-v "$DIR"/redis/data:/data \
 			--restart=always my/redis
       		;;
-      	'mysql') 
-			docker pull registry.docker-cn.com/library/mysql:5.6
+      	'mysql')
+      	    docker build -t my/mysql "$DIR"/mysql
 			resetContainer mysql
 			docker run -d --name mysql --net=myNet \
 			-v "$DIR"/mysql/data:/var/lib/mysql \
 			-v "$DIR"/mysql/my.cnf:/etc/mysql/conf.d/docker.cnf \
 			-e MYSQL_ROOT_PASSWORD=12346 \
 			-p 3306:3306 \
-			--restart=always registry.docker-cn.com/library/mysql:5.6
+			--restart=always my/mysql
       		;;
       	'nginx') 
 			docker build -t my/nginx "$DIR"/nginx
